8fa23ba20af1aa745e926fd328f049a73c3ec4e2
Haripriya211 <60129631+Haripriya211@users.noreply.github.com>
2020-01-22 17:35:26 +0530

    Add files via upload

--- StoreData.java
+++ StoreData.java
@@ -1,29 +1,118 @@
-    package granularity;
-    import org.hibernate.Session;  
-    import org.hibernate.SessionFactory;  
-    import org.hibernate.Transaction;  
-    import org.hibernate.boot.Metadata;  
-    import org.hibernate.boot.MetadataSources;  
-    import org.hibernate.boot.registry.StandardServiceRegistry;  
-    import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
-    public class StoreData {
-	public static void main(String[] args) {
-		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
-		// create meta data object from registry
-		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
-		//build a session factory from meta object    
-		SessionFactory factory = meta.getSessionFactoryBuilder().build();  
-		//create session object from session factory
-		Session session = factory.openSession();  
-		//create transaction object by calling beginTransaction() method of session
-		Transaction t = session.beginTransaction();  
-		Point pt=new Point(5,7,10,2,7,6);
-		double area= pt.CalcArea();
-        Triangle t1=new Triangle(2,"t1",area);
-		session.save(t1);
-		t.commit(); 
-		System.out.println("success...");
-		factory.close(); 
-		session.close();
-	}
-}
+package One2M;
+import java.util.ArrayList;    
+
+import org.hibernate.Session;  
+
+import org.hibernate.SessionFactory;  
+
+import org.hibernate.Transaction;  
+
+import org.hibernate.boot.Metadata;  
+
+import org.hibernate.boot.MetadataSources;  
+
+import org.hibernate.boot.registry.StandardServiceRegistry;  
+
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
+
+public class StoreData {    
+
+public static void main(String[] args) {    
+
+     
+
+    StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+
+    Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();  
+
+      
+
+    SessionFactory factory=meta.getSessionFactoryBuilder().build();  
+
+    Session session=factory.openSession();  
+
+      
+
+    Transaction t=session.beginTransaction();    
+
+        
+
+    Answer ans1=new Answer();    
+
+    ans1.setAnswername("Java is a programming language");    
+
+    ans1.setPostedBy("Susan");    
+
+        
+
+    Answer ans2=new Answer();    
+
+    ans2.setAnswername("Java is a platform independent");    
+
+    ans2.setPostedBy("Gnanitha");    
+
+        
+
+    Answer ans3=new Answer();    
+
+    ans3.setAnswername("Servlet is an Interface");    
+
+    ans3.setPostedBy("Scriven");    
+
+        
+
+    Answer ans4=new Answer();    
+
+    ans4.setAnswername("Servlet is an API");    
+
+    ans4.setPostedBy("Nissy");    
+
+        
+
+    ArrayList<Answer> list1=new ArrayList<Answer>();    
+
+    list1.add(ans1);    
+
+    list1.add(ans2);    
+
+        
+
+    ArrayList<Answer> list2=new ArrayList<Answer>();    
+
+    list2.add(ans3);    
+
+    list2.add(ans4);    
+
+        
+
+    Question question1=new Question();    
+
+    question1.setQname("What is Java?");    
+
+    question1.setAnswers(list1);    
+
+        
+
+    Question question2=new Question();    
+
+    question2.setQname("What is Servlet?");    
+
+    question2.setAnswers(list2);    
+
+        
+
+    session.persist(question1);    
+
+    session.persist(question2);    
+
+        
+
+    t.commit();    
+
+    session.close();    
+
+    System.out.println("success");    
+
+}    
+
+}   
